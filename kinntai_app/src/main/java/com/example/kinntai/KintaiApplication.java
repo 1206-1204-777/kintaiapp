b1bdf274ba1da74b7db83f721c7bfd147a4f5f59
Mamy <xingyanjintian@gmail.com>
2025-05-24 12:47:07 +0900

    Revert "休憩時間の画面表示を行えるように修正をする"
    This reverts commit ac30680a9f62d0421eb5331faf3e3bc889cde0d1.

--- kinntai_app/src/main/java/com/example/kinntai/KintaiApplication.java
+++ kinntai_app/src/main/java/com/example/kinntai/KintaiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class KintaiAppApplication {
+public class KintaiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KintaiAppApplication.class, args);
+		SpringApplication.run(KintaiApplication.class, args);
 		
 		
 	}
--- kinntai_app/src/main/java/com/example/kinntai/service/impl/HolidayServiceImpl.java
+++ kinntai_app/src/main/java/com/example/kinntai/service/impl/HolidayServiceImpl.java
@@ -3,8 +3,6 @@ package com.example.kinntai.service.impl;
 import java.time.LocalDate;
 import java.util.List;
 
-import jakarta.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,22 +18,18 @@ public class HolidayServiceImpl implements HolidayService {
 	private HolidayRepository holidayRepository;
 
 	@Override
-	@Transactional
 	public Holiday registerHoliday(Holiday holiday) {
 		Holiday registerHoliday = holidayRepository.save(holiday);
 		return registerHoliday;
 	}
 
 	@Override
-	@Transactional
 	public List<Holiday> getUsersHoliday(User user, LocalDate startDate, LocalDate endDate) {
-		List<Holiday> holidays = holidayRepository.findByUserAndDateBetweenOrderByDateAsc(user, startDate, endDate);
-		
-		return holidays;
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 
 	@Override
-	@Transactional
 	public boolean checkUsersHoliday(User user, LocalDate date) {
 		return holidayRepository.existsByUserAndDate(user, date);
 	}
